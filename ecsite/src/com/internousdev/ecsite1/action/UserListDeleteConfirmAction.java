b6dedad8ace54d91e57ba9dd996b361e08c3ada6
terada-kaito <taciturnity6101@i.softbank.jp>
2019-04-23 16:20:58 +0900

    4/23

---
+++ ecsite/src/com/internousdev/ecsite1/action/UserListDeleteConfirmAction.java
@@ -0,0 +1,9 @@
+package com.internousdev.ecsite1.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserListDeleteConfirmAction extends ActionSupport {
+	public String execute() {
+		return SUCCESS;
+	}
+}
